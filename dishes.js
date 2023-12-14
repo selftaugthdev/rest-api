6d29940e967c6fefdc5f8e219a582b88b39b83a5
Thierry1980 <thierrydebelder@gmail.com>
2023-12-14 13:31:03 +0100

    Created the dishes in a seperate JS file

---
+++ dishes.js
@@ -0,0 +1,26 @@
+const dishes = [
+    {
+        name: "Spaghetti",
+        timeToPrepare: "30 Minutes",
+        ingredients: "Pasta, Tomatoes, Garlic, Mushrooms",
+        listOfSteps: "Boil the pasta, Cook the tomatoes, add garlic and mushrooms"
+    },
+    {
+        name: "Fish Sticks",
+        timeToPrepare: "10 Minutes",
+        ingredients: "Fish sticks, Mashed potatoes, Spinach",
+        listOfSteps: "Boil the potatoes and mash them, Cook the spinach and add to the potatoes, Bake the fish sticks"
+    },
+    {
+        name: "Meatloaf",
+        timeToPrepare: "60 Minutes",
+        ingredients: "Ground Beef, Spices, Garlic, Unions",
+        listOfSteps: "add all ingredients to the ground beef, Shape the ground beef into an oval ball, Put in the oven for 1 hour"
+    },
+    {
+        name: "Chicken and Rice",
+        timeToPrepare: "30 Minutes",
+        ingredients: "Chicken, Rice, Curry",
+        listOfSteps: "Bake the chicken, Cook the rice and curry"
+    },
+]
